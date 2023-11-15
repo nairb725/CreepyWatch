082e00ee8637e63f62015b26a8a4182c1f496d11
Dylan Andrew RAZAFINTSALAMA <diix.andrew@gmail.com>
2023-11-15 19:43:59 +0100

    feature(menu): create main menu scene

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
