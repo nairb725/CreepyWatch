4cf0933a6eb040328c791baa6ddfd15c5e509a43
Brian <58400498+nairb725@users.noreply.github.com>
2024-01-16 11:43:22 +0100

    timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,30 +3,40 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-
     private float _startTime;
 
     [SerializeField]
     private TMP_Text m_TimerText;
 
     [SerializeField]
+    private float TimerCountMax;
+
+    private float TimeLeft;
+    [SerializeField]
     private bool _isTimer = true;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        _startTime = Time.time;
+        TimeLeft = TimerCountMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_isTimer == true)
+        if (_isTimer && TimeLeft > 0)
         {
-            float currentTime = Time.time - _startTime;
-            int minute = Mathf.FloorToInt(currentTime / 60F);
-            int second = Mathf.FloorToInt(currentTime - minute * 60);
-            m_TimerText.text = string.Format("{0:0}:{1:00}", minute, second);
+            TimeLeft = Mathf.Clamp(TimerCountMax - (Time.time - _startTime), 0f, TimerCountMax);
+
+            UpdateTimerText();
         }
+    }
+
+    void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(TimeLeft / 60F);
+        int seconds = Mathf.FloorToInt(TimeLeft - minutes * 60);
+
+        m_TimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
