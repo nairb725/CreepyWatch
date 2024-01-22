0b86e7ed821c042b25378de02e1d3523f99079c4
Dobrota <dobrotakevin@yahoo.fr>
2024-01-22 14:53:42 +0100

    blinking light

--- Assets/Scripts/BlinkLight.cs
+++ Assets/Scripts/BlinkLight.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class BlinkLight : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Light light;
+    public float minIntensity = 0.5f;
+    public float maxIntensity = 1.0f;
+    public float speed = 1.0f;
+    public AudioSource lightSource;
+
+    public float minTime = 0.5f;
+    public float maxTime = 1.0f;
+    public float time;
+
+    private float intensity;
+
     void Start()
     {
-        
+        intensity = Random.Range(0.0f, 65535.0f);
+        time = Random.Range(minTime, maxTime);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        LightFlickering();
     }
+
+    void LightFlickering()
+    {
+        if (time > 0)
+        {
+            float noise = Mathf.PerlinNoise(intensity, Time.time * speed);
+            light.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+        }
+
+        if (time <= 0)
+        {
+            time = Random.Range(minTime, maxTime);
+            intensity = Random.Range(0.0f, 65535.0f);
+            lightSource.Play();
+        }
+
+    }
+
+
+
 }
